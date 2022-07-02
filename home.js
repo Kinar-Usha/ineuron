25fdd35d1ef95bc9b61cb9b93fb11aec081858f5
Kinar-Usha <kinar.cool@gmail.com>
2022-07-02 20:49:58 +0530

    test

--- home.js
+++ home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useWindowDimensions } from "react-native";
 import {
     Image, Box, Center, Text, View, AspectRatio, HStack, IconButton,
     MaterialIcons,
@@ -9,14 +10,18 @@ import {
     Flex,
     Divider,
     Stack,
-    VStack
+    VStack,
 } from "native-base";
 import { Platform } from 'react-native';
 
 import MyCard from "./card";
 
 export default class Home extends React.Component {
+
     render() {
+    // const windowWidth = useWindowDimensions().width;
+    // console.log(windowWidth);
+
         return (
             <Box w={['100%', '100%', '100%']}>
                 {/* <Box safeAreaTop /> */}
@@ -47,7 +52,7 @@ export default class Home extends React.Component {
                 <VStack>
                     <View>
                         <Heading fontSize={Platform.OS == 'web' ? '5xl' : '2xl'} alignItems={Platform.OS == 'web' ? 'flex-start' : 'center'} justifyItems={Platform.OS == 'web' ? 'flex-start' : 'center'} style={{ marginLeft: Platform.OS === 'web' ? '5%' : 0, marginTop: Platform.OS == 'web' ? '10%' : '5%' }}>
-                            A platform for the{" "}
+                            A platform for the{" "} 
                             <Heading fontSize={Platform.OS == 'web' ? '5xl' : '2xl'} alignItems={Platform.OS == 'web' ? 'flex-start' : 'center'} color="emerald.400">Agricultural Ecosystem</Heading>
                         </Heading>
                         <Text pt="3" fontSize={Platform.OS == 'web' ? 'xl' : 'md'} style={{ marginLeft: Platform.OS === 'web' ? '5%' : 0, marginTop: '1%' }}>
@@ -57,6 +62,7 @@ export default class Home extends React.Component {
                 </VStack>
                 <Flex space={3} justifyContent='space-between' direction={Platform.OS == 'web' ? 'row' : 'column'} marginTop={Platform.OS=='android'?'5%':'10%'} marginLeft={Platform.OS=='android'?'0%':'2%'}>
                     <MyCard title="First One" subtitle="Some random sentence" desc="Bengaluru (also called Bangalore) is the center of India's high-tech industry. The city is also known for its parks and nightlife"/>
+                    Spacer
                     <MyCard title="First One" subtitle="Some random sentence" desc="Bengaluru (also called Bangalore) is the center of India's high-tech industry. The city is also known for its parks and nightlife"/>
                     <MyCard title="First One" subtitle="Some random sentence" desc="Bengaluru (also called Bangalore) is the center of India's high-tech industry. The city is also known for its parks and nightlife"/>
                 </Flex>
